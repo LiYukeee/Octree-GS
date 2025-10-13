5e429c1ea018e9ebf6243107a58f164d0e16f4d3
Yuke Li <liyuke@shanghaitech.edu.cn>
2025-10-13 14:41:20 +0800

    Initial Commit

---
+++ render.py
@@ -0,0 +1,201 @@
+#
+# Copyright (C) 2023, Inria
+# GRAPHDECO research group, https://team.inria.fr/graphdeco
+# All rights reserved.
+#
+# This software is free for non-commercial, research and evaluation use 
+# under the terms of the LICENSE.md file.
+#
+# For inquiries contact  george.drettakis@inria.fr
+#
+import os
+from os import makedirs
+import torch
+import numpy as np
+
+from utils.system_utils import autoChooseCudaDevice
+autoChooseCudaDevice()
+
+from scene import Scene
+import json
+import time
+from gaussian_renderer import render, prefilter_voxel
+import torchvision
+from tqdm import tqdm
+from utils.general_utils import safe_state
+from argparse import ArgumentParser
+from arguments import ModelParams, PipelineParams, get_combined_args
+from gaussian_renderer import GaussianModel
+
+
+def render_set_for_FPS_test(model_path, name, iteration, views, gaussians, pipeline, background, show_level, ape_code):
+    """
+    input: Keep the same input parameters as render_set(...)
+    output: the output is a more accurate FPS.
+    """
+    t_list_len = 1000
+    t_list = np.array([1.0] * t_list_len)
+    step = 0
+    fps_list = []
+    while True:
+        for view in views:
+            step += 1
+            torch.cuda.synchronize();
+            t0 = time.time()
+            gaussians.set_anchor_mask(view.camera_center, iteration, view.resolution_scale)
+            voxel_visible_mask = prefilter_voxel(view, gaussians, pipeline, background)
+            render_pkg = render(view, gaussians, pipeline, background, visible_mask=voxel_visible_mask, ape_code=ape_code)
+            rendering = render_pkg["render"]
+            torch.cuda.synchronize();
+            t1 = time.time()
+            t_list[step % t_list_len] = t1 - t0
+
+            if step % 100 == 0 and step > t_list_len:
+                fps = 1.0 / t_list.mean()
+                print(f'Test FPS: \033[1;35m{fps:.5f}\033[0m')
+                fps_list.append(fps)
+            if step > t_list_len * 2:
+                # write fps info to a txt file
+                with open(os.path.join(model_path, "point_cloud", "iteration_{}".format(iteration), "FPS.txt"), 'w') as f:
+                    f.write("Average FPS: {:.5f}\n".format(np.mean(fps_list)))
+                    f.write("FPS std: {:.5f}\n".format(np.std(fps_list)))
+                print("Average FPS: {:.5f}, FPS std: {:.5f}".format(np.mean(fps_list), np.std(fps_list)))
+                return
+            
+
+def render_set(model_path, name, iteration, views, gaussians, pipeline, background, show_level, ape_code):
+    render_path = os.path.join(model_path, name, "ours_{}".format(iteration), "renders")
+    makedirs(render_path, exist_ok=True)
+    gts_path = os.path.join(model_path, name, "ours_{}".format(iteration), "gt")
+    makedirs(gts_path, exist_ok=True)
+    if show_level:
+        render_level_path = os.path.join(model_path, name, "ours_{}".format(iteration), "renders_level")
+        makedirs(render_level_path, exist_ok=True)
+
+    t_list = []
+    per_view_dict = {}
+    per_view_level_dict = {}
+    for idx, view in enumerate(tqdm(views, desc="Rendering progress")):
+
+        torch.cuda.synchronize(); t0 = time.time()
+
+        gaussians.set_anchor_mask(view.camera_center, iteration, view.resolution_scale)
+        voxel_visible_mask = prefilter_voxel(view, gaussians, pipeline, background)
+        render_pkg = render(view, gaussians, pipeline, background, visible_mask=voxel_visible_mask, ape_code=ape_code)
+        
+        torch.cuda.synchronize(); t1 = time.time()
+        t_list.append(t1-t0)
+
+        rendering = torch.clamp(render_pkg["render"], 0.0, 1.0)
+        visible_count = render_pkg["visibility_filter"].sum()  
+        per_view_dict['{0:05d}'.format(idx)+".png"] = visible_count.item()
+
+        gt = view.original_image[0:3, :, :]
+        torchvision.utils.save_image(rendering, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
+        torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
+
+        if show_level:
+            for cur_level in range(gaussians.levels):
+                gaussians.set_anchor_mask_perlevel(view.camera_center, view.resolution_scale, cur_level)
+                voxel_visible_mask = prefilter_voxel(view, gaussians, pipeline, background)
+                render_pkg = render(view, gaussians, pipeline, background, visible_mask=voxel_visible_mask, ape_code=ape_code)
+                
+                rendering = render_pkg["render"]
+                visible_count = render_pkg["visibility_filter"].sum()
+                
+                torchvision.utils.save_image(rendering, os.path.join(render_level_path, '{0:05d}_LOD{1:d}'.format(idx, cur_level) + ".png"))
+                per_view_level_dict['{0:05d}_LOD{1:d}'.format(idx, cur_level) + ".png"] = visible_count.item()
+
+    t = np.array(t_list[5:])
+    fps = 1.0 / t.mean()
+    print(f'Test FPS: \033[1;35m{fps:.5f}\033[0m')
+
+    with open(os.path.join(model_path, name, "ours_{}".format(iteration), "per_view_count.json"), 'w') as fp:
+        json.dump(per_view_dict, fp, indent=True) 
+    if show_level:
+        with open(os.path.join(model_path, name, "ours_{}".format(iteration), "per_view_count_level.json"), 'w') as fp:
+            json.dump(per_view_level_dict, fp, indent=True)     
+
+
+def render_video(args, model_path, name, iteration, views, gaussians, pipeline, background, show_level, ape_code):
+    n_fames = args.n_frames
+    fps = 30
+    height = views[0].image_height
+    width = views[0].image_width
+    traj_dir = os.path.join(args.model_path, 'traj', "ours_{}".format(iteration))
+    os.makedirs(traj_dir, exist_ok=True)
+    print(f"rendering video to {traj_dir}, n_frames={n_fames}, fps={fps}, height={height}, width={width}")
+    
+    from utils.render_utils import generate_path
+    import cv2
+    cam_traj = generate_path(views, n_frames=n_fames)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    video = cv2.VideoWriter(os.path.join(traj_dir, "render_traj_color.mp4"), fourcc, fps, (width, height))
+    
+    for view in tqdm(cam_traj, desc="Rendering video"):
+        gaussians.set_anchor_mask(view.camera_center, iteration, view.resolution_scale)
+        voxel_visible_mask = prefilter_voxel(view, gaussians, pipeline, background)
+        render_pkg = render(view, gaussians, pipeline, background, visible_mask=voxel_visible_mask, ape_code=ape_code)
+        rendering = render_pkg["render"]
+        frame = rendering.cpu().permute(1, 2, 0).numpy()
+        frame = (frame * 255).astype(np.uint8)
+        frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        video.write(frame_bgr)
+    video.release()
+    
+ 
+def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParams, skip_train : bool, skip_test : bool, show_level : bool, ape_code : int):
+    with torch.no_grad():
+        gaussians = GaussianModel(
+            dataset.feat_dim, dataset.n_offsets, dataset.fork, dataset.use_feat_bank, dataset.appearance_dim, 
+            dataset.add_opacity_dist, dataset.add_cov_dist, dataset.add_color_dist, dataset.add_level, 
+            dataset.visible_threshold, dataset.dist2level, dataset.base_layer, dataset.progressive, dataset.extend
+        )
+        scene = Scene(dataset, gaussians, load_iteration=iteration, shuffle=False, resolution_scales=dataset.resolution_scales)
+        gaussians.eval()
+        gaussians.plot_levels()
+        if dataset.random_background:
+            bg_color = [np.random.random(),np.random.random(),np.random.random()] 
+        elif dataset.white_background:
+            bg_color = [1.0, 1.0, 1.0]
+        else:
+            bg_color = [0.0, 0.0, 0.0]
+        background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
+        if not os.path.exists(dataset.model_path):
+            os.makedirs(dataset.model_path)
+            
+        if args.video:
+            render_video(args, dataset.model_path, "train", scene.loaded_iter, scene.getTrainCameras(), gaussians, pipeline, background, show_level, ape_code)
+
+        if not skip_train:
+            render_set(dataset.model_path, "train", scene.loaded_iter, scene.getTrainCameras(), gaussians, pipeline, background, show_level, ape_code)
+
+        if not skip_test:
+            render_set(dataset.model_path, "test", scene.loaded_iter, scene.getTestCameras(), gaussians, pipeline, background, show_level, ape_code)
+        
+        render_set_for_FPS_test(dataset.model_path, "train", scene.loaded_iter, scene.getTrainCameras(), gaussians, pipeline, background, show_level, ape_code)
+
+if __name__ == "__main__":
+    # Set up command line argument parser
+    parser = ArgumentParser(description="Testing script parameters")
+    model = ModelParams(parser, sentinel=True)
+    pipeline = PipelineParams(parser)
+    parser.add_argument("--iteration", default=-1, type=int)
+    parser.add_argument("--ape", default=10, type=int)
+    parser.add_argument("--skip_train", action="store_true")
+    parser.add_argument("--skip_test", action="store_true")
+    parser.add_argument("--quiet", action="store_true")
+    parser.add_argument("--show_level", action="store_true")
+    parser.add_argument("--video", action="store_true")
+    parser.add_argument("--n_frames", default=300, type=int)
+    args = get_combined_args(parser)
+    print("Rendering " + args.model_path)
+
+    # Initialize system state (RNG)
+    safe_state(args.quiet)
+
+    render_sets(model.extract(args), args.iteration, pipeline.extract(args), args.skip_train, args.skip_test, args.show_level, args.ape)
+    
+    from metrics import *
+    print("Evaluating results...")
+    evaluate([args.model_path])
